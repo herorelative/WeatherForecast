4262d8c5c2619a52236c432080ff9f984ceeb97c
Zar Ni Myo <zarnimyo@sourcedinternational.com>
2021-07-18 23:30:37 +0630

    Add project files.

---
+++ WeatherForecast/wwwroot/scripts/bweInterop.js
@@ -0,0 +1,21 @@
+﻿var bweInterop = {};
+
+bweInterop.getPosition = async function () {
+    function getPositionAsync() {
+        return new Promise((source, error) => {
+            navigator.geolocation.getCurrentPosition(source, error);
+        });
+    }
+
+    if (navigator.geolocation) {
+        var position = await getPositionAsync();
+
+        var coords = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        };
+        return coords;
+    } else {
+        throw Error("Geolocation is not supported by this browser.");
+    }
+}
---
+++ WeatherForecast/wwwroot/service-worker.js
@@ -0,0 +1,40 @@
+﻿const OFFLINE_VERSION = 1;
+const CACHE_PREFIX = 'offline';
+const CACHE_NAME = `${CACHE_PREFIX}${OFFLINE_VERSION}`;
+const OFFLINE_URL = 'offline.html';
+
+self.addEventListener("install", event => event.waitUntil(onInstall(event)));
+
+async function onInstall(event) {
+    console.info('Service worker: Install');
+
+    const cache = await caches.open(CACHE_NAME);
+
+    await cache.add(new Request(OFFLINE_URL));
+}
+
+self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
+
+async function onActivate(event) {
+    console.info('Service worker: Activate');
+
+    const cacheKeys = await caches.keys();
+
+    await Promise.all(cacheKeys.filter(key => key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME).map(key => caches.delete(key)));
+}
+
+self.addEventListener('fetch', event => event.waitUntil(onFetch(event)));
+
+async function onFetch(event) {
+    console.info('Service worker: Fetch');
+
+    if (event.request.method === 'GET') {
+        try {
+            return await fetch(event.request);
+        } catch (error) {
+            const cache = await caches.open(CACHE_NAME);
+
+            return await cahce.match(OFFLINE_URL);
+        }
+    }
+}
